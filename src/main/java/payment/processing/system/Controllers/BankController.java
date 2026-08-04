291e11be58b2aa4e9432f09fc03350301ce7f2cc
BhargaviDasanooruSrinivasa <dsbhargavi105@gmail.com>
2026-08-04 18:48:19 +0000

    Project modifications

---
+++ src/main/java/payment/processing/system/Controllers/BankController.java
@@ -0,0 +1,44 @@
+package payment.processing.system.Controllers;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import payment.processing.project.DTO.Request.BankRequest;
+import payment.processing.project.DTO.Response.BankResponse;
+import payment.processing.project.Services.BankService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/banks")
+@RequiredArgsConstructor
+@Tag(name = "Bank", description = "Bank management APIs")
+public class BankController {
+
+    private final BankService bankService;
+
+    @PostMapping
+    @Operation(summary = "Register a new bank")
+    public ResponseEntity<BankResponse> createBank(@Valid @RequestBody BankRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(bankService.createBank(request));
+    }
+
+    @GetMapping
+    @Operation(summary = "List all banks")
+    public ResponseEntity<List<BankResponse>> getAllBanks() {
+        return ResponseEntity.ok(bankService.getAllBanks());
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get a bank by id")
+    public ResponseEntity<BankResponse> getBank(@PathVariable Long id) {
+        return ResponseEntity.ok(bankService.getBankById(id));
+    }
+}
+
+
